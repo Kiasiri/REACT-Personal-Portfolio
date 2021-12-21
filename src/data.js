3b82fa9937a95fe48eb95814f36ad0c29d3fa3c7
Matthew Wiessing <matthewwiessing@gmail.com>
2021-12-21 17:24:48 -0600

    made data file for project info and updated package.json

---
+++ src/data.js
@@ -0,0 +1,28 @@
+export const projects = [
+  {
+    title: "Note taker",
+    description:
+      "This project demonstrtes saving user input as data to a database and calling that data",
+    deployed: "https://kiasiri.github.io/Note-Taker/",
+    github: "https://github.com/Kiasiri/Note-Taker",
+  },
+  {
+    title: "Password Generator",
+    description: "Javascript code to generate a safe, randomized password",
+    deployed: "https://kiasiri.github.io/password-generator/",
+    github: "https://github.com/Kiasiri/password-generator",
+  },
+  {
+    title: "Weather Forecast App",
+    description: "A weekly forecaster for any city in the US.",
+    deployed: "https://kiasiri.github.io/Weather-Forecast-App/",
+    github: "https://github.com/Kiasiri/Weather-Forecast-App",
+  },
+  {
+    title: "Stream This",
+    description:
+      "This app searches through a couple apis to find the streaming service of a user search parameters.",
+    deployed: "https://jgrmshelton.github.io/GroupPorject_1_StreamThis/#",
+    github: "https://github.com/jgrmshelton/GroupPorject_1_StreamThis",
+  },
+];
