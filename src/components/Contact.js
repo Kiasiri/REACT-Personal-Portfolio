95cb1ff324649fdab176aae70c9c6df72f214ed8
Matthew Wiessing <matthewwiessing@gmail.com>
2021-12-21 19:19:06 -0600

    added function to contact following example in activities

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,68 @@
 import React from "react";
 
 export default function Contact() {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMesssage] = React.useState("");
+  const [error, setError] = React.useState("");
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www.form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => alert("Your message has been sent!"))
+      .catch((error) => alert(error));
+  }
+
   return (
-    <form className="">
+    <form className="" name="contact">
       <h2 className="">Contact Me Here</h2>
-      <p className="">Use the form below to send me a message.</p>
+      <p className="">Use the form below to send a message.</p>
       <div className="">
         <label htmlFor="name" className="">
-          Name
+          Name:
         </label>
-        <input type="text" id="name" name="name" className="" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className=""
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <div className="">
         <label htmlFor="email" className="">
-          Email
+          Email:
         </label>
-        <input type="email" id="email" className="" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className=""
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="">
-        <label htmlFor="" className="">
-          Message
+        <label htmlFor="message" className="">
+          Message:
         </label>
-        <textarea id="" className="" />
+        <textarea
+          id="message"
+          name="message"
+          className=""
+          onChange={(e) => setMesssage(e.target.value)}
+        />
       </div>
       <button type="submit" className="">
         Submit
